73985ac780004a8f4ad8ea58826c4a9653e19c50
gygyuszi0 <you@example.com>
2024-12-24 09:08:54 +0100

    initial commit

---
+++ src/main/java/com/nye/sokoban_web/SokobanWebApplication.java
@@ -0,0 +1,13 @@
+package com.nye.sokoban_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokobanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokobanWebApplication.class, args);
+	}
+
+}
