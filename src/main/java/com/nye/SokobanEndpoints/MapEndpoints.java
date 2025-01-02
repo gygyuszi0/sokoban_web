5d6943a9e56a4c2efa7f4f17e54f6441527297c3
gygyuszi0 <you@example.com>
2025-01-02 11:34:58 +0100

    Align endpoints to dto #1

--- src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
@@ -7,28 +7,27 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-// import com.nye.SokobanApp.AppDto.MapRequest;
-// import com.nye.SokobanApp.AppDto.MapResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.ReadMapResponse;
 
-// import jakarta.validation.Valid;
+import jakarta.validation.Valid;
 
-// @RestController
-// public class MapEndpoints {
+@RestController
+public class MapEndpoints {
 
-    @PostMapping("/create_map")
-    MapResponse createMap(){
-        return new MapResponse();
-    }
+    // @PostMapping("/create_map")
+    // MapResponse createMap(){
+    //     return new MapResponse();
+    // }
     @GetMapping("/read_map/{id}")
-    MapResponse readMap(@PathVariable("id") Long id){
-        return  new MapResponse();
+    ReadMapResponse readMap(@PathVariable("id") Long id){
+        return  new ReadMapResponse();
     }
-    @PutMapping("/update_map/{id}")
-    MapResponse updateMap(@PathVariable("id") Long id, @RequestBody @Valid MapRequest newMap){
-        return new MapResponse();
-    }
-    @DeleteMapping("/delete_map/{id}")
-    MapResponse deleteMap(@PathVariable("id") Long id){
-        return new MapResponse();
-    }
+    // @PutMapping("/update_map/{id}")
+    // MapResponse updateMap(@PathVariable("id") Long id, @RequestBody @Valid MapRequest newMap){
+    //     return new MapResponse();
+    // }
+    // @DeleteMapping("/delete_map/{id}")
+    // MapResponse deleteMap(@PathVariable("id") Long id){
+    //     return new MapResponse();
+    // }
 }
