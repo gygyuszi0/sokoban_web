908fb8726e72a28ca4c1bccea1254ef9e148071a
gygyuszi0 <you@example.com>
2025-01-02 12:22:51 +0100

    update endpoints #2

--- src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
@@ -7,27 +7,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nye.SokobanApp.AppDto.AppRequest.AppMapRequest.CreateMapRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppMapRequest.UpdateMapRequest;
+import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.CreateMapResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.DeleteMapResponse;
 import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.ReadMapResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.UpdateMapResponse;
 
 import jakarta.validation.Valid;
 
 @RestController
 public class MapEndpoints {
 
-    // @PostMapping("/create_map")
-    // MapResponse createMap(){
-    //     return new MapResponse();
-    // }
+    @PostMapping("/create_map")
+    CreateMapResponse createMap(@RequestBody @Valid CreateMapRequest newMap){
+        return new CreateMapResponse();
+    }
     @GetMapping("/read_map/{id}")
     ReadMapResponse readMap(@PathVariable("id") Long id){
         return  new ReadMapResponse();
     }
-    // @PutMapping("/update_map/{id}")
-    // MapResponse updateMap(@PathVariable("id") Long id, @RequestBody @Valid MapRequest newMap){
-    //     return new MapResponse();
-    // }
-    // @DeleteMapping("/delete_map/{id}")
-    // MapResponse deleteMap(@PathVariable("id") Long id){
-    //     return new MapResponse();
-    // }
+    @PutMapping("/update_map/{id}")
+    UpdateMapResponse updateMap(@PathVariable("id") Long id, @RequestBody @Valid UpdateMapRequest newMap){
+        return new UpdateMapResponse();
+    }
+    @DeleteMapping("/delete_map/{id}")
+    DeleteMapResponse deleteMap(@PathVariable("id") Long id){
+        return new DeleteMapResponse();
+    }
 }
