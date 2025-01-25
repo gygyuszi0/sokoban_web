f3e219eef3dd1bafa3f5c6fac7c02f591265a001
gygyuszi0 <you@example.com>
2025-01-25 10:54:59 +0100

    refactor

--- src/main/java/com/nye/SokobanApp/AppDto/AppUserGetter.java
+++ src/main/java/com/nye/SokobanApp/AppDto/AppUserGetter.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppUser {
-    String passWord;
-    String realName;
+public class AppUserGetter {
+    String userName;
 }
