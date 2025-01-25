911a3feb3d74ac1a190566ea66b6fe1097360945
gygyuszi0 <you@example.com>
2025-01-25 10:49:53 +0100

    app_user alap osztály

---
+++ src/main/java/com/nye/SokobanApp/AppDto/AppUser.java
@@ -0,0 +1,11 @@
+package com.nye.SokobanApp.AppDto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppUser {
+    
+}
