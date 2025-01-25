f90ddf76bddf985c70bf222c0bd905d2d07b835d
gygyuszi0 <you@example.com>
2025-01-25 10:19:23 +0100

    gitignor, elérési utak javítása

--- src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/MapEndpoints.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nye.SokobanApp.AppDto.AppRequest.AppMapRequest.CreateMapRequest;
@@ -17,22 +18,27 @@ import com.nye.SokobanApp.AppDto.AppResponse.AppMapResponse.UpdateMapResponse;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/map")
 public class MapEndpoints {
 
-    @PostMapping("/create_map")
+    @PostMapping("/create")
     CreateMapResponse createMap(@RequestBody @Valid CreateMapRequest newMap){
         return new CreateMapResponse();
     }
-    @GetMapping("/read_map/{id}")
+    @GetMapping("/read/{id}")
     ReadMapResponse readMap(@PathVariable("id") Long id){
         return  new ReadMapResponse();
     }
-    @PutMapping("/update_map/{id}")
+    @PutMapping("/update/{id}")
     UpdateMapResponse updateMap(@PathVariable("id") Long id, @RequestBody @Valid UpdateMapRequest newMap){
         return new UpdateMapResponse();
     }
-    @DeleteMapping("/delete_map/{id}")
+    @DeleteMapping("/delete/{id}")
     DeleteMapResponse deleteMap(@PathVariable("id") Long id){
         return new DeleteMapResponse();
+    }
+    @GetMapping("ping")
+    Integer ping(){
+        return 0;
     }
 }
