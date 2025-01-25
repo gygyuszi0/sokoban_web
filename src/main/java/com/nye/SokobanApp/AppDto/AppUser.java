52a24a12054099ac68cd081094cf2b3dab1d1468
gygyuszi0 <you@example.com>
2025-01-25 10:53:37 +0100

    user adatok

--- src/main/java/com/nye/SokobanApp/AppDto/AppUser.java
+++ src/main/java/com/nye/SokobanApp/AppDto/AppUser.java
@@ -1,11 +1,14 @@
 package com.nye.SokobanApp.AppDto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppUser {
-    
+    String passWord;
+    String userName;
+    String realName;
 }
