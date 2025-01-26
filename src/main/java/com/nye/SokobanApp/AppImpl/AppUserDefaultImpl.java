0a13c556c2a3f740895a898710e2d7df677aae06
gygyuszi0 <you@example.com>
2025-01-26 10:46:58 +0100

    app user impl

---
+++ src/main/java/com/nye/SokobanApp/AppImpl/AppUserDefaultImpl.java
@@ -0,0 +1,38 @@
+package com.nye.SokobanApp.AppImpl;
+
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.CreateUserRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.DeleteUserRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.ReadUserRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.UpdateUserRequest;
+import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppCreateUserResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppReadUserResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppUpdateUserResponse;
+import com.nye.SokobanApp.AppInterface.AppUserInterface;
+
+public class AppUserDefaultImpl implements AppUserInterface {
+
+    @Override
+    public AppCreateUserResponse createUser(CreateUserRequest request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteUser(DeleteUserRequest request) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public AppReadUserResponse readUser(ReadUserRequest request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public AppUpdateUserResponse updateUser(UpdateUserRequest request) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
