050223382df2c59318eec3fe318523d4917a6c6f
gygyuszi0 <you@example.com>
2025-01-26 10:32:40 +0100

    score endpoints

--- src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
@@ -1,10 +1,48 @@
 package com.nye.SokobanEndpoints;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppCreateScoreRequest;
+import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppCreateScoreResponse;
+import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppReadScoreResponse;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppReadScoreRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppUpdateScoreRequest;
+import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppUpdateScoreRespons;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppDeleteScoreRequest;
+
+
+
 
 @RestController
 @RequestMapping("/score")
 public class ScoreEndpoints {
 
+    @PostMapping("/create")
+    public AppCreateScoreResponse createScore(@RequestBody AppCreateScoreRequest score) {
+        // TODO: implement logic to create a new score
+        return new AppCreateScoreResponse();
+    }
+
+    @GetMapping("/read/{id}")
+    public AppReadScoreResponse readScore(@PathVariable Long id) {
+        // TODO: implement logic to read a score
+        return new AppReadScoreResponse();
+    }
+
+    @PutMapping("/update/{id}")
+    public AppReadScoreResponse updateScore(@PathVariable Long id, @RequestBody AppUpdateScoreRequest score){
+        // TODO: implement logic to update a score
+        return new AppReadScoreResponse();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteScore(@PathVariable Long id) {
+        // TODO: implement logic to delete a score
+    }
+        
 }
