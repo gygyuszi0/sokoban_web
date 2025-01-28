47e56e76fc04368dcc28888a5aa8145017fd33eb
gygyuszi0 <you@example.com>
2025-01-28 18:52:50 +0100

    user service meghívása

--- src/main/java/com/nye/SokobanEndpoints/UserEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/UserEndpoints.java
@@ -1,5 +1,6 @@
 package com.nye.SokobanEndpoints;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,35 +10,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.CreateUserRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.DeleteUserRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.ReadUserRequest;
 import com.nye.SokobanApp.AppDto.AppRequest.AppUserRequest.UpdateUserRequest;
 import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppCreateUserResponse;
 import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppReadUserResponse;
 import com.nye.SokobanApp.AppDto.AppResponse.AppUserResponse.AppUpdateUserResponse;
+import com.nye.SokobanApp.AppInterface.AppUserInterface;
 
 @RestController
 @RequestMapping("/user")
 public class UserEndpoints {
 
+    @Autowired
+    private AppUserInterface userService;
+
     @PostMapping("/create")
     public AppCreateUserResponse createUser(@RequestBody CreateUserRequest user) {
-        // TODO: Implement the logic to create a new user
-        return new AppCreateUserResponse();
+        return userService.createUser(user);
     }
 
     @GetMapping("/read/{id}")
     public AppReadUserResponse readUser(@PathVariable Long id) {
-        // TODO: Implement the logic to read a user by id
-        return new AppReadUserResponse();
+        ReadUserRequest request = ReadUserRequest.builder().id(id).build();
+        return userService.readUser(request);
     }
 
     @PutMapping("/update/{id}")
     public AppUpdateUserResponse updateUser(@PathVariable Long id, @RequestBody UpdateUserRequest user) {
-        // TODO: Implement the logic to update a user by id
-        return new AppUpdateUserResponse();
+        user.setId(id);
+        return userService.updateUser(user);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable Long id) {
-        // TODO: Implement the logic to delete a user by id
+        DeleteUserRequest request = DeleteUserRequest.builder().id(id).build();
+        userService.deleteUser(request);
     }
 }
