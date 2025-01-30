67f8d80b250eb50255b040f092f47f1cae09b31a
gygyuszi0 <you@example.com>
2025-01-30 18:14:50 +0100

    score service meghívása

--- src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
@@ -1,7 +1,9 @@
 package com.nye.SokobanEndpoints;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppReadScoreRespon
 import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppReadScoreRequest;
 import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppUpdateScoreRequest;
 import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppUpdateScoreRespons;
+import com.nye.SokobanApp.AppInterface.AppScoreInterface;
 import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppDeleteScoreRequest;
 
 
@@ -22,27 +25,31 @@ import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppDeleteScoreReques
 @RequestMapping("/score")
 public class ScoreEndpoints {
 
+    @Autowired
+    private AppScoreInterface scoreService;
+
     @PostMapping("/create")
     public AppCreateScoreResponse createScore(@RequestBody AppCreateScoreRequest score) {
-        // TODO: implement logic to create a new score
-        return new AppCreateScoreResponse();
+        return scoreService.createScore(score);
     }
 
-    @GetMapping("/read/{id}")
-    public AppReadScoreResponse readScore(@PathVariable Long id) {
-        // TODO: implement logic to read a score
-        return new AppReadScoreResponse();
+    @GetMapping("/read/{map_id}/{user_id}")
+    public AppReadScoreResponse readScore(@PathVariable Long map_id, @PathVariable Long user_id) {
+        AppReadScoreRequest request = AppReadScoreRequest.builder().mapId(map_id).userId(user_id).build();
+        return scoreService.readScore(request);
     }
 
-    @PutMapping("/update/{id}")
-    public AppReadScoreResponse updateScore(@PathVariable Long id, @RequestBody AppUpdateScoreRequest score){
-        // TODO: implement logic to update a score
-        return new AppReadScoreResponse();
+    @PutMapping("/update/{map_id}/{user_id}")
+    public AppReadScoreResponse updateScore(@PathVariable Long map_id, @PathVariable Long user_id, @RequestBody AppUpdateScoreRequest score) {
+        score.setMapId(map_id);
+        score.setUserId(user_id);
+        return scoreService.updateScore(score);
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteScore(@PathVariable Long id) {
-        // TODO: implement logic to delete a score
+        AppDeleteScoreRequest request = AppDeleteScoreRequest.builder().id(id).build();
+        scoreService.deleteScore(request);
     }
         
 }
