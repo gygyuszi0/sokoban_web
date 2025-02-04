e43ca3d79e86104ac18f53ba5b4ea6ae789d96d5
gygyuszi0 <you@example.com>
2025-02-04 18:43:58 +0100

    fix map service

--- src/main/java/com/nye/SokobanApp/AppImpl/AppMapDefaultImpl.java
+++ src/main/java/com/nye/SokobanApp/AppImpl/AppMapDefaultImpl.java
@@ -1,5 +1,9 @@
 package com.nye.SokobanApp.AppImpl;
 
+import com.nye.storage.entity.CoordinateEntity;
+import com.nye.storage.entity.MapEntity;
+import com.nye.storage.service.MapStorage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nye.SokobanApp.AppDto.AppRequest.AppMapRequest.CreateMapRequest;
@@ -14,10 +18,25 @@ import com.nye.SokobanApp.AppInterface.AppMapInterface;
 @Service
 public class AppMapDefaultImpl implements AppMapInterface {
 
+    @Autowired
+    private MapStorage mapStorage;
+
     @Override
     public CreateMapResponse createMap(CreateMapRequest map) {
-        // TODO Auto-generated method stub
-        return new CreateMapResponse();
+        CoordinateEntity coordinate = CoordinateEntity.builder()
+                .x(map.getStartCoordinate().getX())
+                .y(map.getStartCoordinate().getY())
+                .build();
+        MapEntity request = MapEntity.builder()
+                .mapContent(map.getMapContent())
+                .startCoordinateX(coordinate.getX())
+                .startCoordinateY(coordinate.getY())
+                .build();
+        MapEntity response = mapStorage.save(request);
+        CreateMapResponse result = CreateMapResponse.builder().
+                id(response.getId())
+                .build();
+        return result;
     }
 
     @Override
