4f1665cb5d4f850636bc6be59fae24306c82e31f
gygyuszi0 <you@example.com>
2025-02-09 09:19:31 +0100

    navigaton javítása

--- frontend/app/routes/navigation.tsx
+++ frontend/app/routes/navigation.tsx
@@ -3,15 +3,32 @@ import { Link } from "@remix-run/react";
 import React from "react";
 
 
+function EmptyNav() {
+    return (
+        <>
+            <nav className="nav">
+                <div className="nav-item font-bold">Sokoban Web</div>
+            </nav>
+        </>
+    );
+};
+
 export default function Nav() {
+    return (
+        <>
+            <EmptyNav />
+        </>
+    );
+};
+
+
+function LoggedInNav(props) {
     return (
         <>
             <nav className="nav">
                 <div className="nav-item font-bold">Sokoban Web </div>
-                <div className="nav-item">
-                    <Link to="/login">Login</Link>
-                </div>
+                <div className="nav-item">{props.userName} </div>
             </nav>
         </>
     );
-}
+};
