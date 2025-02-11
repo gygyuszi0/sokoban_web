26dbb62c8f1e16675aee05143265dbebab2eefd6
gygyuszi0 <you@example.com>
2025-02-11 18:19:37 +0100

    helyesírás

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -9,7 +9,7 @@ export default function Login() {
             <div className="login-page">
                 <div className="login-window">
                     <h3>Log in</h3>
-                    <Form className="login_from" method="post" action="./proceed">
+                    <Form className="login_form" method="post" action="./proceed">
                         <input type="text" name="username" placeholder="Username" />
                         <input type="password" name="password" placeholder="Password" />
                         <button className="button-style" type="submit" name="submit_login">OK</button>
--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -8,7 +8,7 @@ export default function Register() {
             <div className="register-page">
                 <div className="register-window">
                     <h3>Register</h3>
-                    <Form className="register_from" method="post" action="./proceed">
+                    <Form className="register_form" method="post" action="./proceed">
                         <input type="text" name="user_name" placeholder="Username" />
                         <input type="text" name="real_name" placeholder="Real name" />
                         <input type="password" name="password" placeholder="Password" />
