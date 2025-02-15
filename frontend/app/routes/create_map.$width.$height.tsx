8d6513f867fe1d65fd33353d304a8b2b2aa157b5
gygyuszi0 <you@example.com>
2025-02-15 12:24:04 +0100

    map grid struktúra átszervezése

--- frontend/app/routes/create_map.$width.$height.tsx
+++ frontend/app/routes/create_map.$width.$height.tsx
@@ -23,27 +23,11 @@ function MapRow({ children }: { children: React.ReactNode }) {
     );
 }
 
-function MapItem(props) {
-    const name = "item_" + props.index;
-    const index = props.index;
+
+function StateItem(props) {
+    const label = props.label;
     return (
-        <input className="map-grid-item" type="text" disabled={true} name={name} value="s" />
-    );
-}
-
-const MapGrid = (props) => {
-
-    let rows = [];
-    for (let i = 0; i < props.height; i++) {
-        let row = [];
-        for (let j = 0; j < props.width; j++) {
-            row.push(<MapItem index={i * props.width + j} />);
-        }
-        rows.push(<MapRow>{row}</MapRow>);
-    }
-
-    return (
-        <>{rows}</>
+        <button type="button">{label}</button>
     );
 }
 
@@ -53,14 +37,43 @@ export default function RouteComponent() {
     const height = parseInt(params.height);
 
     const [selected, setSelected] = useState("s");
+    const [buttonLabels, setButtonLabels] = useState([...Array(width * height).keys()].map(i => "s"));
 
+    function MapItem(props) {
+        const name = "item_" + props.index;
+        const index = props.index;
+        const label = buttonLabels[index];
+        return (
+            <button className="map-grid-item" name={name}>{label}</button>
+        );
+    }
+
+    const MapGrid = (props) => {
+
+        let rows = [];
+        for (let i = 0; i < props.height; i++) {
+            let row = [];
+            for (let j = 0; j < props.width; j++) {
+                row.push(<MapItem index={i * props.width + j} />);
+            }
+            rows.push(<MapRow>{row}</MapRow>);
+        }
+    
+        const labels = [...props.labels];
+        return (
+            <>
+                {rows}
+            </>
+        );
+    }
+    
     return (
         <>
             <h1 className="sub-title">Create map</h1>
             <p>Slected : {selected}</p>
             <div className="create-map-content">
                 <div className="map-grid">
-                    <MapGrid width={width} height={height} />
+                    <MapGrid width={width} height={height} labels={buttonLabels} />
                 </div>
 
                 <div className="map-items">
@@ -70,6 +83,12 @@ export default function RouteComponent() {
                         <button className="map-grid-item" onClick={() => (setSelected("p"))}>p</button>
                         <button className="map-grid-item" onClick={() => (setSelected("b"))}>b</button>
                         <button className="map-grid-item" onClick={() => (setSelected("t"))}>t</button>
+                        <button className="map-grid-item" onClick={() => {
+                                let new_array =[...Array(5).keys()].map(i => "s");
+                                new_array[3] = "w";
+                                setButtonLabels(new_array);
+                            }}>{buttonLabels[3]}</button>
+                        
                     </div>
                 </div>
                 <form className="map-buttons" method="post">
