1a596829a37bf3369ac00428d140b48bad8b13b9
gygyuszi0 <you@example.com>
2025-02-18 18:06:02 +0100

    find all score

--- src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
+++ src/main/java/com/nye/SokobanEndpoints/ScoreEndpoints.java
@@ -1,15 +1,24 @@
 package com.nye.SokobanEndpoints;
 
-import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppScoreDeleteRequest;
-import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppUpdateScoreResponse;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppCreateScoreRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppReadScoreRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppScoreDeleteRequest;
+import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppUpdateScoreRequest;
 import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppCreateScoreResponse;
 import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppReadScoreResponse;
-import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppReadScoreRequest;
-import com.nye.SokobanApp.AppDto.AppRequest.AppScoreRequest.AppUpdateScoreRequest;
+import com.nye.SokobanApp.AppDto.AppResponse.AppScoreResponse.AppUpdateScoreResponse;
 import com.nye.SokobanApp.AppInterface.AppScoreInterface;
 
 
@@ -31,6 +40,11 @@ public class ScoreEndpoints {
     public AppReadScoreResponse readScore(@PathVariable Long map_id, @PathVariable Long user_id) {
         AppReadScoreRequest request = AppReadScoreRequest.builder().mapId(map_id).userId(user_id).build();
         return scoreService.readScore(request);
+    }
+
+    @GetMapping("/read_all")
+    public List<AppReadScoreResponse> readScore() {
+        return scoreService.readAll();
     }
 
     @PutMapping("/update/{map_id}/{user_id}")
