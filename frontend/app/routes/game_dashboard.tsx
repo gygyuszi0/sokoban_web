651f61dd8ae327319a9418864e8409f6695d99fb
gyorgyi.gyula <gyorgyi.gyula@miltech.hu>
2025-02-20 13:14:21 +0100

    új dashboard elem

--- frontend/app/routes/game_dashboard.tsx
+++ frontend/app/routes/game_dashboard.tsx
@@ -16,6 +16,10 @@ export default function GameDashboard() {
           <div className="dashboard-item-img">img</div>
           <h2 className="dashboard-item-title">Play</h2>
         </a>
+        <a href="play_map_selector_shift" className="dashboard-item">
+          <div className="dashboard-item-img">img</div>
+          <h2 className="dashboard-item-title">Play with target shift</h2>
+        </a>
         <a className="dashboard-item" href="./create_map_dimensions">   
           <div className="dashboard-item-img">img</div>
           <h2 className="dashboard-item-title">Craete new map</h2>
