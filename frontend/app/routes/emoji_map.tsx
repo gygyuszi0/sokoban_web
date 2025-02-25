4d683e5c7e25b1b653972e6182e710cf930cd6ba
gyorgyi.gyula <gyorgyi.gyula@miltech.hu>
2025-02-25 14:46:54 +0100

    map emoji felvétele #36

--- frontend/app/routes/emoji_map.tsx
+++ frontend/app/routes/emoji_map.tsx
@@ -2,9 +2,10 @@
 
 export const emojiMap = new Map([
     ["p",       String.fromCodePoint(0x1f468)],
-    ["b",       "b"],
-    ["t",       "t"],
-    ["s",       "s"],
+    ["b",       String.fromCodePoint(0x1f4e6)],
+    ["t",       String.fromCodePoint(0x1f7e4)],
+    ["tb",       String.fromCodePoint(0x1f7e9)],
+    ["s",       String()],
     ["right",   "p"],
     ["left",    "p"],
     ["up",      "p"],
