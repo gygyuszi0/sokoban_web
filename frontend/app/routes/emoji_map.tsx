31e87967500d31236ae82d92476885099a1dde98
gyorgyi.gyula <gyorgyi.gyula@miltech.hu>
2025-02-25 12:48:40 +0100

    emoji map vázlat #36

---
+++ frontend/app/routes/emoji_map.tsx
@@ -0,0 +1,12 @@
+
+
+export const emojiMap = new Map([
+    ["p",       "p"],
+    ["b",       "p"],
+    ["t",       "p"],
+    ["s",       "p"],
+    ["right",   "p"],
+    ["left",    "p"],
+    ["up",      "p"],
+    ["down",    "p"],
+]);
