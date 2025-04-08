881b62a275f265baf2168ee0d1d2d92ba1a59796
gygyuszi0 <you@example.com>
2025-04-08 18:55:52 +0200

    sudo javítása

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-sudo su
-
-apt install git 
-apt install npm openjdk-17-jdk postgresql maven
+sudo apt install git -y
+sudo apt install npm openjdk-17-jdk postgresql maven -y
 
 git clone https://github.com/gygyuszi0/sokoban_web.git
 cd sokoban_web
