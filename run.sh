3856765dd4a6e27c9ee44dc5d2f42f793f544906
gygyuszi0 <you@example.com>
2025-04-09 13:08:38 +0200

    wait before frontend

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 
 java -jar target/sokoban-web-1.0-SNAPSHOT-jar-with-dependencies.jar &
 
+sleep 6s
 cd frontend
 npm run dev &
 cd ..
 
-xdg-open http://localhost:5173
+# xdg-open http://localhost:5173
