59bdcd1d112488e745dd389bd2b82b3872da736d
gygyuszi0 <you@example.com>
2025-04-09 19:19:52 +0200

    docker base

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+apt update
+apt install git -y
+apt install npm openjdk-17-jdk postgresql maven -y
+
+#Config backend
+/usr/lib/postgresql/15/bin/ postgres -D /usr/local/pgsql/data >logfile 2>&1 &
+psql -U postgres -d postgres -c "alter user postgres with password 'root';"
+mvn package
+
+#Config frontend
+cd frontend
+npm install
+cd ..
