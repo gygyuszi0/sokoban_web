bbba5ab2a38a61d12fb7ce355eb18f7e89e00e3c
gygyuszi0 <you@example.com>
2025-04-09 12:18:46 +0200

    clone törlése

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt install git -y
 sudo apt install npm openjdk-17-jdk postgresql maven -y
 
-git clone https://github.com/gygyuszi0/sokoban_web.git
-cd sokoban_web
+# git clone https://github.com/gygyuszi0/sokoban_web.git
+# cd sokoban_web
+
 mvn package
